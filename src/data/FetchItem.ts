233422fcb29c001b7e0fb17b2d3daf56cf2165c1
Nira <prunusnira@gmail.com>
2023-03-09 17:15:45 +0900

    item list의 이미지 확장자 변경

--- src/data/FetchItem.ts
+++ src/data/FetchItem.ts
@@ -4,28 +4,28 @@ const dataSetItem = [
         category: "top",
         name: "대충 옷 이름 111",
         price: 100000,
-        image: ["cloth1.png"],
+        image: ["cloth1.jpg"],
     },
     {
         id: 1002,
         category: "golf",
         name: "대충 옷 이름 2222",
         price: 200000,
-        image: ["cloth2.png"],
+        image: ["cloth2.jpg"],
     },
     {
         id: 1003,
         category: "golfwear",
         name: "대충 옷 이름 3333",
         price: 50000,
-        image: ["cloth3.png"],
+        image: ["cloth3.jpg"],
     },
     {
         id: 1004,
         category: "golfwear",
         name: "대충 옷 이름 4444",
         price: 50000,
-        image: ["cloth4.png"],
+        image: ["cloth4.jpg"],
     },
 ];
 
